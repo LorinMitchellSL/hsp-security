114aa4cdd58a9f2c7daaed26f70855247ba67bc1
LorinMitchellSL <lorin-mitchell@outlook.com>
2017-10-18 15:19:54 +0800

    1、Add User Check Model-HspUserDetailService 2、Add Password Encode & Match function-PasswordEncoder etc.

--- hsp-security-browser/src/main/java/com/hand/BrowserSecurityConfig.java
+++ hsp-security-browser/src/main/java/com/hand/BrowserSecurityConfig.java
@@ -1,15 +1,25 @@
 package com.hand;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        //实现类可以根据具体情况进行编写
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception{
 
+//        http.httpBasic() 弹出对话框的SpringSecurity安全认证
         http.formLogin()
                 .and()
                 .authorizeRequests()
---
+++ hsp-security-browser/src/main/java/com/hand/HspUserDetailService.java
@@ -0,0 +1,38 @@
+package com.hand;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HspUserDetailService implements UserDetailsService {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        //此处需要查询数据库
+        logger.info("登陆用户名："+s);
+        //本应将该转换过后的字符串写到数据库中
+        //在登陆时，使用passwordEncoder.matches()方法比对密码
+        String psw = passwordEncoder.encode("123456");
+        logger.info(psw);
+        return new User(s,psw, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+//        return new User("123456",
+//                "123456",
+//                true,
+//                true,
+//                true,
+//                false,
+//                AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+    }
+}

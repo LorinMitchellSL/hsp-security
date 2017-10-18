405bf592eac615fc9bf8a29c929fe256ecff3f6a
LorinMitchellSL <lorin-mitchell@outlook.com>
2017-10-18 11:08:44 +0800

    1、Create Browser Configure 2、modified the application.properties of demo

--- hsp-security-browser/src/main/java/com/hand/BrowserSecurityConfig.java
+++ hsp-security-browser/src/main/java/com/hand/BrowserSecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class BrowerSecurityConfig extends WebSecurityConfigurerAdapter {
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
